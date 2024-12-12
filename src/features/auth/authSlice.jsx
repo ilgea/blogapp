02828c89cbeca615b9a9a6812440848654882d9b
ilgea <zubeyirozkaya@gmail.com>
2024-12-12 12:41:50 +0300

    kayt olurken kullanıcı isminin gözükmemesi sorunu

--- src/features/auth/authSlice.jsx
+++ src/features/auth/authSlice.jsx
@@ -33,8 +33,7 @@ export const registerUser = createAsyncThunk(
         email: user.email,
         photoURL: user.photoURL || null,
       };
-          
-      toastSuccessNotify("Kayıt Başarılı");
+      toastSuccessNotify(`Kayıt Başarılı, Hoşgeldiniz ${user?.displayName}`);
       return userData;
     } catch (error) {
       if (error.code === "auth/email-already-in-use") {
